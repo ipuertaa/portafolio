79a89b47731e7f4610d51c70bcfb81d85084eae2
IsabelPuerta <isabel.mia2244@gmail.com>
2024-04-08 19:24:39 -0500

    Mejoras al menu navegacion
    Se desarrollaron mejoras al menu de navegacion y se comenzó la parte visual

--- navegacion.js
+++ navegacion.js
@@ -1,45 +1,6 @@
-//Función para el menú lateral (modo responsive)
-function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%"
-}
-
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%"
-}
-
-//Función para asignar texto
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML = texto;
-    return;     
-}
-
-//Función para mostrar detalles sección trabajo
-function mostarDetalles(x){
-    switch (x){
-        case 1:
-            asignarTextoElemento('texto-detalles', "TEXTO AWS");
-            break;
-        
-        case 2:
-            asignarTextoElemento('texto-detalles', "TEXTO DESARROLLO WEB");
-            break;
-        
-        case 3:
-            asignarTextoElemento('texto-detalles', "TEXTO PYHTON");
-            break;
-    
-        case 4:
-            asignarTextoElemento('texto-detalles', "TEXTO MICRONCONTROLADORES");
-            break;
-        
-        default:
-            break;
+function closeMenu(){
+    var checkbox = document.getElementById("menu-checkbox");
+    if (checkbox.checked) {
+        checkbox.checked = false; // Desactivar el estado 'checked' del checkbox
     }
-
-    document.getElementById("detalles-trabajo").style.display = "inline"
-}
-
-function closeDetalles(){
-    document.getElementById("detalles-trabajo").style.display = "none"
 }
